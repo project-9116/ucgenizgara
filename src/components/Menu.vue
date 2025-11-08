286a894340eabe13c3b6d674841aa4e6ad5d7e0f
volkantash <djangoogreniyorum@gmail.com>
2025-11-08 21:19:28 +0300

    Menü yazıları düzeltildi.

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,8 +2,8 @@
     <nav>
         <ul class="menu">
             <li><RouterLink to="/sik-sorulan-sorular" class="btn btn-primary">SSS</RouterLink></li>
-            <li><RouterLink to="/iletisim" class="btn btn-primary">İLETİŞİM</RouterLink></li>
-            <li><RouterLink to="/hakkimizda" class="btn btn-primary">HAKKIMIZDA</RouterLink></li>
+            <li><RouterLink to="/iletisim" class="btn btn-primary">İletişim</RouterLink></li>
+            <li><RouterLink to="/hakkimizda" class="btn btn-primary">Hakkımızda</RouterLink></li>
         </ul>
     </nav>
 </template>
