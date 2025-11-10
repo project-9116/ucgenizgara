98835c4220ec5a5ae5192e9be80914a92ec7fcbf
volkantash <djangoogreniyorum@gmail.com>
2025-11-10 19:25:55 +0300

    iletişim bilgileri eklendi.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,23 +1,14 @@
 <template>
   <main>
-    <h1 class="text-4xl mb-10">About</h1>
-    <img
-      class="mb-10 rounded"
-      width="400"
-      height="225"
-      :src="`https://picsum.photos/400/225?${Math.random()}`"
-      alt="Shoes"
-    />
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien
-      diam, sollicitudin hendrerit consequat a, malesuada sed odio. Maecenas
-      convallis fermentum placerat. Phasellus tempus enim vitae facilisis
-      viverra. Maecenas auctor sollicitudin neque eget pharetra. Ut vitae
-      consectetur dui, at interdum justo. In hac habitasse platea dictumst.
-      Phasellus ornare venenatis congue. In accumsan libero a laoreet bibendum.
-      Aliquam sed congue urna. Aliquam at scelerisque odio. Aenean in turpis vel
-      tortor tristique interdum. Vivamus finibus, orci at dapibus fermentum,
-      arcu ante placerat ex, nec tincidunt risus nibh non elit.
-    </p>
+    <article>
+    <h1>Üçgen Izgaranın Mucidi Namık Bey</h1>
+    <p><b>Telefon:</b> +90 533 255 07 60</p>
+    <h1>Satış Yerleri</h1>
+    <h2>Hüralp Yapı Market</h2>
+    <p><b>Telefon:</b> +90 506 797 52 75</p>
+    <p><b>Adres:</b> Hüralp Yapı Market - Atatürk Mah. 858 Sk. No: 38 Bornova / İZMİR</p>
+    <p><b>Satış:</b> <a href="https://huralp.com/ucgen-izgara-ocakustu-pratik-izgara">huralp.com: Üçgen Izgara</a></p>
+
+    </article>
   </main>
 </template>
