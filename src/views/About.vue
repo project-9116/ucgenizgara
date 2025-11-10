e9bc37b6db3799de33b390c3d0bc2394b7c1ac97
volkantash <djangoogreniyorum@gmail.com>
2025-11-11 00:33:28 +0300

    İletişim sayfasında içerik teması düzeltildi.

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,10 +2,10 @@
   <main>
     <article>
     <h1>Üçgen Izgaranın Mucidi</h1>
-    <h2>Namık Kemal KARAMUK</h2>
+    <h3>Namık Kemal KARAMUK</h3>
     <p><b>Telefon:</b> +90 533 255 07 60</p>
     <h1>Satış Yerleri</h1>
-    <h2>Hüralp Yapı Market</h2>
+    <h3>Hüralp Yapı Market</h3>
     <p><b>Telefon:</b> +90 506 797 52 75</p>
     <p><b>Adres:</b> Hüralp Yapı Market - Atatürk Mah. 858 Sk. No: 38 Bornova / İZMİR</p>
     <p><b>Satış:</b> <a href="https://huralp.com/ucgen-izgara-ocakustu-pratik-izgara">huralp.com: Üçgen Izgara</a></p>
